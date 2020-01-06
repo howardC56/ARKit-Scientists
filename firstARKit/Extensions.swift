cf3f4ea1b7e11fdebf6a454133bfad4b6d085291
howardC56 <h2chang56@gmail.com>
2020-01-05 21:26:25 -0500

    Initial Commit

---
+++ firstARKit/Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  Extensions.swift
+//  firstARKit
+//
+//  Created by Howard Chang on 1/5/20.
+//  Copyright © 2020 Howard Chang. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+extension SCNNode {
+    var width: Float {
+        return (boundingBox.max.x - boundingBox.min.x) * scale.x
+    }
+    
+    var height: Float {
+        return (boundingBox.max.y - boundingBox.min.y) * scale.y
+    }
+    
+    func pivotOnTopLeft() {
+        let (min,max) = boundingBox
+        pivot = SCNMatrix4MakeTranslation(min.x, (max.y - min.y) + min.y, 0)
+    }
+    
+    func pivotOnTopCenter() {
+        let (min,max) = boundingBox
+        pivot = SCNMatrix4MakeTranslation((max.x - min.x) / 2 + min.x, (max.y - min.y) + min.y, 0)
+    }
+}
---
+++ firstARKit/Scientist.swift
@@ -0,0 +1,18 @@
+//
+//  Scientist.swift
+//  firstARKit
+//
+//  Created by Howard Chang on 1/5/20.
+//  Copyright © 2020 Howard Chang. All rights reserved.
+//
+
+import Foundation
+
+struct Scientist: Decodable {
+    let name: String
+    let dates: String
+    let field: String
+    let bio: String
+    let country: String
+    let source: String
+}
